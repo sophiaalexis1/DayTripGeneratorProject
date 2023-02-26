46280b731ef9ae827a0ec563be8d561b572b60b4
Sophia Jean-Pierre <sophiajean-pierre@sophias-imac.lan>
2023-02-26 17:42:40 -0500

    finished main function

--- main.py
+++ main.py
@@ -1,54 +1,77 @@
-#Creating lists
 places = ['Atlanta', 'Washington D.C.', 'New York City', 'Austin', 'Boston', 'Seattle', 'Los Angeles']
-# print(place)
 
 restaurants = ['Chick-fil-a', 'Poké Bar', 'Dominoes', 'Burger King', 'Zaxbys', 'Panda Express', 'Taco Bell']
-# print(restaurants)
 
 mode_of_transportation = ['Private Car', 'Bus', 'Subway', 'Helicopter', 'Airplane', 'Limousine', 'Taxi']
-# print(mode_of_transportation)
 
 entertainments = ['Karaoke', 'Movies', 'Top Golf', 'Bowling', 'Arcade', 'Concert', 'Muesum', 'Festival']
-# print(entertainment)
 
 import random
-
+valid_response = False
 def get_destination():
     destination = random.choice(places)
     return destination
-
-# destination = get_destination()
-# # print("Destination:",destination)
 
 def get_restaurant():
     restaurant = random.choice(restaurants)
     return restaurant
 
-# restaurant = get_restaurant()
-# # print("Restaurant:",restaurant)
-
-def get_transporation():
-    transporation = random.choice(mode_of_transportation)
-    return transporation
-
-# transporation = get_transporation()
-# # print("Transporation:",transporation)
+def get_transportation():
+    transportation = random.choice(mode_of_transportation)
+    return transportation
 
 def get_entertainment():
     entertainment = random.choice(entertainments)
     return entertainment
-
-# entertainment = get_entertainment()
-# # print("Entertainment:",entertainment)
 
 def run_day_trip_generator():
     destination = get_destination()
     print("Destination:",destination)
     restaurant = get_restaurant()
     print("Restaurant:",restaurant)
-    transporation = get_transporation()
-    print("Transporation:",transporation)
+    transportation = get_transportation()
+    print("Transporation:",transportation)
     entertainment = get_entertainment()
     print("Entertainment:",entertainment)
-
+    while valid_response == False:
+        satisfaction = input("Are you satisfied with your trip? Y or N ")
+        if satisfaction == "N":
+            user_input = input("Which option would you like to change? Destination, Restaurant, Transportation, or Entertainment? ")
+            if user_input == "Destination":
+                destination = get_destination()
+                print("Destination:",destination)
+                print("Restaurant:",restaurant)
+                print("Transportation:",transportation)
+                print("Entertainment:",entertainment)
+            elif user_input == "Restaurant":
+                restaurant = get_restaurant()
+                print("Destination:",destination)
+                print("Restaurant:",restaurant)
+                print("Transportation:",transportation)
+                print("Entertainment:",entertainment)
+            elif user_input == "Transportation":
+                transportation = get_transportation()
+                print("Destination:",destination)
+                print("Restaurant:",restaurant)
+                print("Transportation:",transportation)
+                print("Entertainment:",entertainment)
+            elif user_input == "Entertainment":
+                 entertainment = get_entertainment()
+                 print("Destination:",destination)
+                 print("Restaurant:",restaurant)
+                 print("Transportation:",transportation)
+                 print("Entertainment:",entertainment)
+            else:
+                print("I do not understand, please try again")
+        elif satisfaction == "Y":
+            print("Here if your final trip!")
+            print("Destination:",destination)
+            print("Restaurant:",restaurant)
+            print("Transportation:",transportation)
+            print("Entertainment:",entertainment)
+            break
+        else:
+            print("Please type in Y or N. ")
+       
 run_day_trip_generator()
+
