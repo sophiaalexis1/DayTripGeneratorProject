7650eb6ac1a0e504046ba223791989ec2254c2fd
Sophia Jean-Pierre <sophiajean-pierre@sophias-imac.lan>
2023-02-26 16:16:23 -0500

    first set of functions

--- main.py
+++ main.py
@@ -1,12 +1,42 @@
 #Creating lists
-destination = ['Atlanta', 'Washington D.C.', 'New York City', 'Austin', 'Boston', 'Seattle', 'Los Angeles']
-print(destination)
+places = ['Atlanta', 'Washington D.C.', 'New York City', 'Austin', 'Boston', 'Seattle', 'Los Angeles']
+# print(place)
 
 restaurants = ['Chick-fil-a', 'Poké Bar', 'Dominoes', 'Burger King', 'Zaxbys', 'Panda Express', 'Taco Bell']
-print(restaurants)
+# print(restaurants)
 
 mode_of_transportation = ['Private Car', 'Bus', 'Subway', 'Helicopter', 'Airplane', 'Limousine', 'Taxi']
-print(mode_of_transportation)
+# print(mode_of_transportation)
 
-entertainment = ['Karaoke', 'Movies', 'Top Golf', 'Bowling', 'Arcade', 'Concert', 'Muesums', 'Festival']
-print(entertainment)
+entertainments = ['Karaoke', 'Movies', 'Top Golf', 'Bowling', 'Arcade', 'Concert', 'Muesum', 'Festival']
+# print(entertainment)
+
+import random
+
+def get_destination():
+    destination = random.choice(places)
+    return destination
+
+destination = get_destination()
+print("Destination:",destination)
+
+def get_restaurant():
+    restaurant = random.choice(restaurants)
+    return restaurant
+
+restaurant = get_restaurant()
+print("Restaurant:",restaurant)
+
+def get_transporation():
+    transporation = random.choice(mode_of_transportation)
+    return transporation
+
+transporation = get_transporation()
+print("Transporation:",transporation)
+
+def get_entertainment():
+    entertainment = random.choice(entertainments)
+    return entertainment
+
+entertainment = get_entertainment()
+print("Entertainment:",entertainment)
