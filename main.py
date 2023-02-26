9ab81044ece783d5d0ef7bde6064df8cca7df280
Sophia Jean-Pierre <sophiajean-pierre@sophias-imac.lan>
2023-02-26 15:52:33 -0500

    created lists and main file

---
+++ main.py
@@ -0,0 +1,12 @@
+#Creating lists
+destination = ['Atlanta', 'Washington D.C.', 'New York City', 'Austin', 'Boston', 'Seattle', 'Los Angeles']
+print(destination)
+
+restaurants = ['Chick-fil-a', 'Poké Bar', 'Dominoes', 'Burger King', 'Zaxbys', 'Panda Express', 'Taco Bell']
+print(restaurants)
+
+mode_of_transportation = ['Private Car', 'Bus', 'Subway', 'Helicopter', 'Airplane', 'Limousine', 'Taxi']
+print(mode_of_transportation)
+
+entertainment = ['Karaoke', 'Movies', 'Top Golf', 'Bowling', 'Arcade', 'Concert', 'Muesums', 'Festival']
+print(entertainment)
